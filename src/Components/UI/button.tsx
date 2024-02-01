81b7c3705cc236c3846f977e4105e17b00db970b
Hron0 <sasha1254370@gmail.com>
2024-02-01 19:36:28 +0300

    Changed colors (a bit)

--- src/Components/UI/button.tsx
+++ src/Components/UI/button.tsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90 rounded-md",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90 rounded-sm font-semibold",
+          "bg-destructive text-destructive-foreground hover:bg-green-500/50 rounded-sm font-semibold",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
